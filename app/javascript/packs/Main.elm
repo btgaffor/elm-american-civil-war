4d71bac12e77bb7ce3aee98c411e6e93cba024c1
Brandon Gafford <brandon.gafford@gmail.com>
2017-10-29 20:45:01 -0700

    autoformat Main

--- app/javascript/packs/Main.elm
+++ app/javascript/packs/Main.elm
@@ -1,33 +1,36 @@
 module Main exposing (..)
 
 import Html exposing (Html)
--- import Mouse exposing (..)
-import Window
 
-import Model exposing (..)
-import Update exposing (..)
-import View exposing (..)
+
+-- import Mouse exposing (..)
+
+import Window
+import Model exposing (Model)
+import Update exposing (Action(WindowResize), init, update)
+import View exposing (view)
+
 
 -- VIEW
-
 -- ACTION
-
-
 -- SUBSCRIPTIONS
+
 
 subscriptions : Model -> Sub Action
 subscriptions model =
-  Window.resizes (\size -> WindowResize size)
-  -- Mouse.moves (\position -> MouseMove position.x position.y)
+    -- Mouse.moves (\position -> MouseMove position.x position.y)
+    Window.resizes (\size -> WindowResize size)
+
+
 
 -- MAIN
 
+
 main : Program Never Model Action
 main =
-  Html.program
-    {
-      init = init,
-      view = view,
-      update = update,
-      subscriptions = subscriptions
-    }
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
